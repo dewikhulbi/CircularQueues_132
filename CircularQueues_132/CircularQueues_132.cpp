1a04379f548e57264b516a54f5a301999b19aeaa
dewikhulbi <dewikhlb20@gmail.com>
2023-06-22 15:47:08 +0700

    menampilkan "Check for the values entered" di layar

--- CircularQueues_132/CircularQueues_132.cpp
+++ CircularQueues_132/CircularQueues_132.cpp
@@ -124,8 +124,12 @@ int main() {
 			}
 			default: {
 				cout << "Invalid option!!" << endl;
+				break;
 			}
 			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
 		}
 	}
 }
