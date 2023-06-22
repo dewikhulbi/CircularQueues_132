17ea7fa22189b34305f11c4b38dda26e8f0fd158
dewikhulbi <dewikhlb20@gmail.com>
2023-06-22 15:36:20 +0700

    membuat int main untuk menjalankan program

--- CircularQueues_132/CircularQueues_132.cpp
+++ CircularQueues_132/CircularQueues_132.cpp
@@ -79,8 +79,14 @@ public:
 		else {
 			//jika FRONT_position <=REAr_position,iterasi dari FRONT hingga akhir array
 			while (FRONT_position <= max - 1) {
-
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
 			}
+			cout << endl;
 		}
 	}
 };
+
+int main() {
+
+}
