42195d3527bb51b192386555ccefdbb070446cca
dewikhulbi <dewikhlb20@gmail.com>
2023-06-22 15:24:33 +0700

    menampilkan "queue is empty" di layar

--- CircularQueues_132/CircularQueues_132.cpp
+++ CircularQueues_132/CircularQueues_132.cpp
@@ -60,5 +60,10 @@ public:
 	void display() {
 		int FRONT_position = FRONT;
 		int REAR_position = REAR;
+
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is emptyt\n";
+		}
 	}
 };
