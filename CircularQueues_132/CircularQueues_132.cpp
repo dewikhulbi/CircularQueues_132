1e2aac0b6b79091315e5f7acb5dc872ba330a443
dewikhulbi <dewikhlb20@gmail.com>
2023-06-22 15:29:07 +0700

    jika FRONT_position <=REAr_position,iterasi diurutkan dari FRONT hingga REAR

--- CircularQueues_132/CircularQueues_132.cpp
+++ CircularQueues_132/CircularQueues_132.cpp
@@ -71,8 +71,10 @@ public:
 		//jika FRONT_position <=REAr_position,iterasi dari FRONT hingga REAR
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position <= REAR_position) {
-				
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
 			}
+			cout << endl;
 		}
 	}
 };
