2879bd075db715ffa3b77ab6750cf143df0c593e
dewikhulbi <dewikhlb20@gmail.com>
2023-06-22 15:27:49 +0700

    membuat pengulangan while

--- CircularQueues_132/CircularQueues_132.cpp
+++ CircularQueues_132/CircularQueues_132.cpp
@@ -67,5 +67,12 @@ public:
 			return;
 		}
 		cout << "\nElements in the queue are...\n";
+
+		//jika FRONT_position <=REAr_position,iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				
+			}
+		}
 	}
 };
